67d0fe2049c4cf8d08ebc300b5d2f89596d70024
景淮jinghuai <jinghuai8092@gmail.com>
2021-07-19 16:35:10 -0400

    code optimization

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
+import TodoItem from "./todoItem";
 import './style.css';
+
 
 class TodoList extends Component {
     constructor(props) {
@@ -13,6 +15,9 @@ class TodoList extends Component {
                 'Learn Chinese'
             ]
         }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
     }
     render() {
         return (
@@ -20,56 +25,61 @@ class TodoList extends Component {
                 {/* Fragment是一个占位符，替代最外层包裹全部的div */}
                 <div>
                     <label htmlFor='insertArea'> Input:</label>
-                    
+
                     <input
                         id='insertArea'
                         className='input'
                         value={this.state.inputValue}
-                        onChange={this.handleInputChange.bind(this)}
+                        onChange={this.handleInputChange}
                     // bind可以设置绑定this指向
                     />
-                    <button onClick={this.handleBtnClick.bind(this)}>Submit</button>
+                    <button onClick={this.handleBtnClick}>Submit</button>
                 </div>
                 <ul>
-                    {
-                        this.state.list.map((item, index) => {
-                            return <li key={index} onClick={this.handleItemDelete.bind(this, index)}
-                            dangerouslySetInnerHTML={
-                                {__html:item}
-                            }
-                            ></li>
-                        })
-                    }
+                    {this.getTodoItem()}
                 </ul>
             </Fragment>
         )
     }
-    handleInputChange(e) {
-
-        // this.state.inputValue=e.target.value 无法直接赋值
-        this.setState({
-            inputValue: e.target.value
+    getTodoItem(){
+        return this.state.list.map((item, index) => {
+            return (
+                    <TodoItem
+                        key={index}
+                        content={item}
+                        index={index}
+                        deleteItem={this.handleItemDelete}
+                    />
+            )
         })
     }
+
+    handleInputChange(e) {
+        const value=e.target.value
+        this.setState(()=>({
+                inputValue:value
+        }))
+        // this.state.inputValue=e.target.value 无法直接赋值
+    }
+
     handleBtnClick() {
-        this.setState({
-            list: [
-                ...this.state.list,
-                this.state.inputValue
+        this.setState((prevState)=>({
+            list:[
+                ...prevState.list,
+                prevState.inputValue
             ],
             inputValue: ''
-        })
+        }))
     }
     handleItemDelete(index) {
 
         // immutable
         //state 不允许我们做任何的改变
         // 下面是拷贝了一个副本出来，然后再赋值
-        
-        const list = [...this.state.list]
-        list.splice(index, 1)
-        this.setState({
-            list:list
+        this.setState((prevState)=>{
+            const list=[...prevState.list];
+            list.splice(index,1);
+            return {list}
         })
         // console.log(index)
 
